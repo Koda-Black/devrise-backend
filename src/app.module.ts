a6038496aeea7d0835a0a6899f2edad8e4975e77
kodaBlack <79699841+Koda-Black@users.noreply.github.com>
2025-01-06 21:26:26 +0100

    added the blog module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NinjasModule } from './ninjas/ninjas.module';
 import { UsersModule } from './users/users.module';
+import { BlogModule } from './blog/blog.module';
 
 @Module({
-  imports: [NinjasModule, UsersModule],
+  imports: [NinjasModule, UsersModule, BlogModule],
   controllers: [AppController],
   providers: [AppService],
 })
