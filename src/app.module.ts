099388555440086d1b108645d1f94b6f0be4547b
kodaBlack <79699841+Koda-Black@users.noreply.github.com>
2025-01-07 20:40:43 +0100

    added the articles module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { NinjasModule } from './ninjas/ninjas.module';
 import { UsersModule } from './users/users.module';
 import { BlogModule } from './blog/blog.module';
+import { ArticlesModule } from './articles/articles.module';
 
 @Module({
-  imports: [NinjasModule, UsersModule, BlogModule],
+  imports: [NinjasModule, UsersModule, BlogModule, ArticlesModule],
   controllers: [AppController],
   providers: [AppService],
 })
